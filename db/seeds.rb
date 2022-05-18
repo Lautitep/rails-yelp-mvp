eb3677a8be2dff5d2c18f35300f5da283c878f5b
Laura Petit <lau.petit0@gmail.com>
2022-05-18 16:15:53 +0200

    complete yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating 5 restaurants..."
+dishoom = { name: "Dishoom", address: "Londres", phone_number: "+44 20 7420 9320", category: "chinese" }
+bigmamma =  { name: "Big Mamma", address: "Paris", phone_number: "+331 86 47 78 34", category: "italian" }
+etage =  { name: "L'étage", address: "Annecy", phone_number: "+334 50 51 03 28", category: "french" }
+lapetitecour =  { name: "La petite cour", address: "Lille", phone_number: "+333 20 51 52 81", category: "french" }
+fritas = { name: "La fritas", address: "Bruxelles", phone_number: "+32 2 539 44 54", category: "belgian" }
+
+[ dishoom, bigmamma, etage, lapetitecour, fritas ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
